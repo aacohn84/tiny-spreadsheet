9d336cde071712617c2b9f31cac37fedab4044c0
Aaron Cohn <aacohn84@gmail.com>
2014-02-20 12:51:58 -0800

    Start TopSorter

---
+++ TinySpreadsheet/include/core/TopSorter.h
@@ -0,0 +1,21 @@
+/*
+ * TopSorter.h
+ *
+ *  Created on: Feb 20, 2014
+ *      Author: Aaron Cohn
+ */
+
+#ifndef TOPSORTER_H_
+#define TOPSORTER_H_
+
+namespace core {
+
+class TopSorter {
+public:
+	TopSorter();
+	virtual ~TopSorter();
+};
+
+} /* namespace core */
+
+#endif /* TOPSORTER_H_ */
---
+++ TinySpreadsheet/src/core/TopSorter.cpp
@@ -0,0 +1,18 @@
+/*
+ * TopSorter.cpp
+ *
+ *  Created on: Feb 20, 2014
+ *      Author: Aaron Cohn
+ */
+
+#include "core/TopSorter.h"
+
+namespace core {
+
+TopSorter::TopSorter() {
+
+}
+
+TopSorter::~TopSorter() {}
+
+} /* namespace core */
